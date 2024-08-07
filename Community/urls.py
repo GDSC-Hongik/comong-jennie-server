00e5250fa3e5cb2b729d5d28560e8ec4d5504010
Nahoseung <wbc2104@g.hongik.ac.kr>
2024-08-07 15:11:27 +0900

    crawler.py 수정

--- Community/urls.py
+++ Community/urls.py
@@ -1,25 +1,29 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-# from .views import grade,sub,prof,posts,post_detail
-from .views import post_detail,post_create,grade_post ,sub_post , prof_post , notice_view , join_post_list,join_post_detail,join_post_create
+from .views import post_detail,post_create,grade_post ,majorboard_view,sub_post , prof_post , main_view , join_post_list,join_post_detail,join_post_create
 app_name = "Community"
 
 
 
 urlpatterns = [
+    # 메인 화면(공지사항, 각 게시판 이동 경로)
+    path('',main_view, name = 'main'),
+    
     # 전공 게시판
-    path('major/<int:grade>/', grade_post, name ='grade-post'),
+    path('major/',majorboard_view, name = 'majorboard-view'),
+    path('major/<int:grade>/', grade_post, name= 'grade-post'),
     path('major/<int:grade>/<str:sub>/',sub_post, name = 'sub-post'),
     path('major/<int:grade>/<str:sub>/<str:profs>/',prof_post,name = 'prof-post'),
     path('major/<int:grade>/<str:sub>/<str:profs>/<int:post_pk>/',post_detail.as_view(), name = 'post-detail'),
     path('major/<int:grade>/<str:sub>/<str:profs>/create/', post_create.as_view() , name = 'post-create'),
+    
     # 구인 게시판
-    path('join/',join_post_list,name = 'join-post-list'),
+    path('join/',join_post_list, name = 'join-post-list'),
     path('join/<int:post_pk>/',join_post_detail.as_view(),name = 'join-post-detail'),
     path('join/create/',join_post_create.as_view(),name = 'join -post-create'),
     
-    # 학과 공지사항
-    path('notice/',notice_view, name = 'notice')
+    
+    
     
 ]
