f2176848f7562ad8dbe3655011fa7ea4949cb256
Nahoseung <wbc2104@g.hongik.ac.kr>
2024-08-15 01:36:41 +0900

    구인 게시판 구인  인원 증감 기능 추가

--- Community/models.py
+++ Community/models.py
@@ -45,7 +45,8 @@ class Sub_post(Post):
 class Join_post(Post):
     participants_num = models.IntegerField(null=True)
     current_num = models.IntegerField(null=True)
-    
+
+
 class Comment(models.Model):
     id = models.AutoField(primary_key=True,null=False,blank=False)
     sub_post = models.ForeignKey(Sub_post,null=False,blank=False,on_delete=models.CASCADE) # Post의 id와 Comment의 post는 동일값을 가짐
@@ -55,7 +56,13 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.comment
-
+# migration yet
+class Join_comment(models.Model):
+    id = models.AutoField(primary_key=True,null=False,blank=False)
+    user = models.ForeignKey(User,null=False,blank=False,on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True,null=False,blank=False)
+    join_post = models.ForeignKey(Join_post,null=False,blank=False,on_delete=models.CASCADE) # Post의 id와 Comment의 post는 동일값을 가짐
+        
 class likes(models.Model):
     like = models.IntegerField(null=True,default=0)
     sub_post = models.ForeignKey(Sub_post,null=True,blank=False,on_delete=models.CASCADE)
